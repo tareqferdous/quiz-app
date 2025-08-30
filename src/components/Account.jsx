df2ceff6ef912635d1e8992c8c1f735e39715792
tareqferdous <16101017@uap-bd.edu>
2025-08-30 12:39:42 +0600

    homepage ui complete

---
+++ src/components/Account.jsx
@@ -0,0 +1,12 @@
+import { FaRegCircleUser } from "react-icons/fa6";
+import classes from "../styles/Account.module.css";
+
+export default function Account() {
+  return (
+    <div className={classes.account}>
+      <FaRegCircleUser />
+      <a href="signup.html">Signup</a>
+      {/* <span className="material-icons-outlined" title="Logout"> logout </span> */}
+    </div>
+  );
+}
